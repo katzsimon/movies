822cb40183e556b6e3dee7cdd8e065e462d7f9c3
Katzsimon <katzsimon@gmail.com>
2021-03-11 18:36:22 +0200

    test: Setting up base Tests

---
+++ _packages/katzsimon/base/tests/BaseCRUDTest.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Tests;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+abstract class BaseCRUDTest extends TestCase
+{
+
+
+    /**
+     * Abstract class to extend for Resource Model CRUD testing
+     */
+
+    use DatabaseTransactions;
+
+    protected $user = null;
+    protected $model = null;
+    protected $table = null;
+    protected $baseUrl = null;
+
+    protected $createdModel = null;
+
+    /**
+     * Setup function to run before tests
+     */
+    public function setUp():void
+    {
+        parent::setUp();
+
+        $this->table = $this->model->getTable();
+        $this->setupUser();
+        config()->set('settings.output.admin', 'json');
+    }
+
+
+    /**
+     * Create a user for authentication if needed
+     *
+     * Uses existing user model if exists, otherwise creates a new one
+     */
+    protected function setupUser()
+    {
+        $user = \App\Models\User::first();
+
+        if (empty($user)) {
+            $this->user = \App\Models\User::factory()->create();
+        } else {
+            $this->user = $user;
+        }
+
+        $this->actingAs($this->user);
+    }
+
+
+    /**
+     * Check that the index function works
+     */
+    public function testIndex()
+    {
+
+        $items = $this->model->factory(5)->create();
+
+        // Check index :: Get the json response from index and check if the above factory is in it
+        $response = $this->get($this->baseUrl)->assertStatus(200);
+        $data = $response->json()['items']??[];
+
+
+        // Check that the returned data from the index, has at least as many items created by the factory
+        $this->assertGreaterThanOrEqual(count($data), count($items));
+
+    }
+
+
+    /**
+     * Check that the Store function works
+     */
+    public function testStore() {
+
+        $item = $this->model->factory()->make();
+
+        // Check create :: Create model
+        $response = $this->postJson($this->baseUrl, $item->toArray(), ['FORCE_CONTENT_TYPE'=>'json'])
+            ->assertStatus(200);
+
+
+        // Check that the database contains the item created by the factory
+        $this->assertDatabaseHas($this->table, $item->toArray());
+    }
+
+
+    /**
+     * Check that the update function works
+     */
+    public function testUpdate()
+    {
+
+        $item = $this->model->factory()->create();
+        $updatedItem = $this->model->factory()->make();
+
+        // Removes the dates for comparison
+        $itemArray = $item->toArray();
+        unset($itemArray['created_at']);
+        unset($itemArray['updated_at']);
+
+        // Check that the database contains the factory item
+        $this->assertDatabaseHas($this->table, $itemArray);
+
+        // Update the item with the second factory updatedItem
+        $response = $this->patch("{$this->baseUrl}/{$item->getKey()}", $updatedItem->toArray(), ['FORCE_CONTENT_TYPE'=>'json'])
+            ->assertStatus(200);
+
+        // Check that the original item does not exist anymore
+        $this->assertDatabaseMissing($this->table, $itemArray);
+        // Check that the updatedItem now exists in the database
+        $this->assertDatabaseHas($this->table, $updatedItem->toArray());
+
+
+    }
+
+
+    /**
+     * Check the Show function, if it exists
+     */
+
+    public function testShow()
+    {
+
+        $item = $this->model->factory()->create();
+
+        $response = $this->get("{$this->baseUrl}/{$item->getKey()}", [], ['FORCE_CONTENT_TYPE'=>'json']);
+        $status = $response->status();
+
+        if ($status===200) {
+
+            try {
+                $data = $response->json();
+                $data = $data['item'] ?? $data['data'] ?? [];
+
+                // $fieldsCompared should be empty if the attributes in item match attributes in the returned data item
+                $fieldsCompared = array_diff($item->toArray(), $data);
+                $this->assertTrue(empty($fieldsCompared), 'Model equivalent to Show item');
+            } catch (\Exception $e) {
+                // show route is probably not defined
+                $this->assertTrue(true, 'Show route does not exist');
+            }
+
+        } elseif ($status===404) {
+            // Response did not return the model
+            $this->assertTrue(false, 'Model not found');
+        } elseif ($status===500) {
+            // Show method does not exist in the Controller
+            $this->assertTrue(true, 'Show method does not exist');
+        }
+
+
+    }
+
+    /**
+     * Check that the Destroy function works
+     */
+    public function testDestroy()
+    {
+        $item = $this->model->factory()->create();
+
+        // Removes the dates for comparison
+        $itemArray = $item->toArray();
+        unset($itemArray['created_at']);
+        unset($itemArray['updated_at']);
+
+        // Check that the model is in the database
+        $this->assertDatabaseHas($this->table, $itemArray);
+
+        $response = $this->delete("{$this->baseUrl}/{$item->getKey()}", [], ['FORCE_CONTENT_TYPE'=>'json'])
+            ->assertStatus(200);
+
+        // The model should not exist in the database now
+        $this->assertDatabaseMissing($this->table, $itemArray);
+
+    }
+
+
+}
---
+++ _packages/katzsimon/base/tests/BaseRepositoryTest.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+abstract class BaseRepositoryTest extends TestCase
+{
+
+
+    /**
+     * Abstract class to extend for Resource Model CRUD testing
+     */
+
+    use DatabaseTransactions;
+
+
+    protected $repository = null;
+    protected $skipBaseTests = false;   // Allow these base tests to be skipped, if needed
+    protected $truncateTables = true;
+
+
+
+    /**
+     * Setup function to run before tests
+     */
+    public function setUp():void
+    {
+        parent::setUp();
+
+    }
+
+
+    public function testModel()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        $model = $this->repository->model();
+
+        $this->assertTrue($model instanceof \Illuminate\Database\Eloquent\Model, 'Model function does not return an Eloquent model');
+    }
+
+    /**
+     * Test creating a new Model
+     */
+    public function testCreate()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        // Make Model data
+        $item = $this->repository->model()->factory()->make();
+        // Save the data to the database
+        $model = $this->repository->create($item->toArray());
+
+        // Compare that the model data matches the database model
+        $this->assertTrue( $this->matchModels($item, $model) );
+    }
+
+    /**
+     * Test that updating a model works
+     */
+    public function testUpdate()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        // Create the original Model
+        $item = $this->repository->model()->factory()->create();
+        // Make new Model data (without saving to the database) **
+        $updatedItem = $this->repository->model()->factory()->make();
+
+        // Save the original MOdel values in an array
+        $original = $item->toArray();
+
+        // Update the original Model
+        $model = $this->repository->update($item, $updatedItem->toArray());
+
+        // Check that the original does not match the new Model
+        $this->assertFalse( $this->matchArrays($original, $model->toArray()) );
+        // Check that the new Model matches the new Model data **
+        $this->assertTrue( $this->matchModels($updatedItem, $model) );
+    }
+
+    /**
+     * Test that the find method, with the Models defined key
+     * default key = id
+     */
+    public function testFind()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        $item = $this->repository->model()->factory()->create();
+
+        $model = $this->repository->find($item->getKey());
+
+        $this->assertTrue( $this->matchModels($item, $model) );
+    }
+
+    /**
+     * Test that the limit method only returns a subset of items
+     */
+    public function testLimit()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        $this->repository->model()->factory(5)->create();
+
+        $items = $this->repository->limit(2);
+
+        $this->assertEquals(count($items), 2);
+
+    }
+
+
+    /**
+     * Test that the empty method returns a model with attributes, but they have no values
+     */
+    public function testEmpty()
+    {
+        if ($this->skipBaseTests) $this->markTestSkipped('Skipping Base Tests');
+
+        $item = $this->repository->empty();
+
+        $this->assertTrue( $this->hasEmptyAttributes($item) );
+
+    }
+
+
+    /**
+     * Compare 2 Models by checking if all the attributes in the Source exist in the Target
+     * Once a Model is created/saved, new attributes can exist, so this takes that into account for comparison
+     *
+     * @param $source
+     * @param $target
+     * @return bool
+     */
+    public function matchModels($source, $target)
+    {
+        $attributes = $source->getAttributes();
+
+        foreach ($attributes as $key=>$value) {
+            if ($source->$key!=$target->$key) return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Compares 2 Arrays, by checking that all fields in the Source exist in the Target
+     * Returns true as long as the source fields exist in the target,
+     * Target can have additional fields
+     *
+     * @param $source
+     * @param $target
+     * @return bool
+     */
+    public function matchArrays($source, $target)
+    {
+        if (!is_array($source) || !is_array($target)) return false;
+        // Get the difference between the source and target
+        $fieldsCompared = array_diff($source, $target);
+        // If empty than all source fields exist in target
+        return empty($fieldsCompared);
+    }
+
+    /**
+     * Checks that all the attributes in a Model are empty
+     *
+     * @param $source
+     * @return bool
+     */
+    public function hasEmptyAttributes($source)
+    {
+        $attributes = $source->getAttributes();
+        foreach ($attributes as $attribute) {
+            if (!empty($attribute)) return false;
+        }
+        return true;
+    }
+
+}
